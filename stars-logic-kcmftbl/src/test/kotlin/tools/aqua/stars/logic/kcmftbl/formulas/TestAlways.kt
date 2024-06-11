d0d1c995a0e7f12e65100bba71a028faa297648f
Fiona Kullmann <fiona.kullmann@tu-dortmund.de>
2024-06-11 14:26:49 +0200

    add predicate to dsl, rename globally to always

--- stars-logic-kcmftbl/src/test/kotlin/tools/aqua/stars/logic/kcmftbl/formulas/TestAlways.kt
+++ stars-logic-kcmftbl/src/test/kotlin/tools/aqua/stars/logic/kcmftbl/formulas/TestAlways.kt
@@ -23,7 +23,7 @@ import kotlin.test.assertFailsWith
 import tools.aqua.stars.logic.kcmftbl.*
 
 /** This class tests the CMFTBL operator [globally]. */
-class TestGlobally {
+class TestAlways {
 
   /**
    * Test equivalence to eventually.
